5a21683f53afb60825b7e2730eaeb53b3f4ed9ff
Fernandez Ludovic <ldez@users.noreply.github.com>
2020-12-11 19:28:08 +0100

    fix: examples method names.

--- country_test.go
+++ country_test.go
@@ -10,17 +10,12 @@ import (
 var query *Query
 
 func TestMain(m *testing.M) {
-	var err error
-
-	if err != nil {
-		panic(err)
-	}
 	query = New()
 	flag.Parse()
 	os.Exit(m.Run())
 }
 
-func ExampleBorderingCountries() {
+func ExampleCountry_BorderingCountries() {
 
 	se, _ := query.FindCountryByAlpha("SWE")
 	for _, country := range se.BorderingCountries() {
@@ -33,7 +28,7 @@ func ExampleBorderingCountries() {
 
 }
 
-func ExampleTranslations() {
+func ExampleCountry_Translations() {
 
 	se, _ := query.FindCountryByAlpha("SWE")
 	fmt.Println(se.Translations["DEU"].Common)
